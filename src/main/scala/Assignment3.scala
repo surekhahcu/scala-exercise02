bde5686561983bf227bdcea985e07cdc88216564
Suri1596 <surekha.k@techsophy.com>
2019-01-17 18:46:12 +0530

    Assignment 03

--- src/main/scala/Assignment3.scala
+++ src/main/scala/Assignment3.scala
@@ -2,26 +2,58 @@ package edu.hcu.assignment3
 
 class Assignment3 {
 
+  //Insertion sort
   def insertion(arr: Array[Int]): Array[Int] = {
-    ???
-  }
-
-  def bubble(arr: Array[Int]): Array[Int] = {
-    for (i <- 0 to arr.length) {
-      for (j <- 0 to i) {
-        if (arr(j) > arr(j + 1)) {
-          arr(j) = arr(j) + arr(j + 1)
-          arr(j + 1) = arr(j) - arr(j + 1)
-          arr(j) = arr(j) - arr(j + 1)
-        }
-
+    for (i <- 1 to arr.length - 1) {
+      var j = i
+      while ((j > 0) && (arr(j - 1) > arr(j))) {
+        arr(j - 1) = arr(j - 1) + arr(j)
+        arr(j) = arr(j - 1) - arr(j)
+        arr(j - 1) = arr(j - 1) - arr(j)
+        j = j - 1
       }
     }
-
+    arr
   }
 
+
+  // Bubble sort
+  def bubble(arr: Array[Int]): Array[Int] = {
+    for (i <- 0 to arr.length - 1) {
+      for (j <- 1 to arr.length - 1) {
+        if (arr(j - 1) > arr(j)) {
+          arr(j - 1) = arr(j - 1) + arr(j)
+          arr(j) = arr(j - 1) - arr(j)
+          arr(j - 1) = arr(j - 1) - arr(j)
+        }
+      }
+    }
+    arr
+  }
+
+ 
+
+
+
+
+  //Selection sort
   def selection(arr: Array[Int]): Array[Int] = {
-    ???
+    for (i <- 0 to arr.length - 1) {
+      var position = i
+      for (j <- i + 1 to arr.length - 1) {
+        if (arr(position) > arr(j)) {
+          position = j
+        }
+      }
+      if (position != i) {
+        arr(position) = arr(position) + arr(i)
+        arr(i) = arr(position) - arr(i)
+        arr(position) = arr(position) - arr(i)
+      }
+    }
+    arr
   }
+
+  //Merge sort
 
 }
--- src/test/scala/Assignment3Test.scala
+++ src/test/scala/Assignment3Test.scala
@@ -6,7 +6,11 @@ class Assignment3Test extends FunSuite {
   val obj = new Assignment3
 
   //1st
-  //2nd
+  test("Insertion sort") {
+    val result = obj.insertion(Array(12, 11, 13, 5, 6))
+    val expectResult = Array(5, 6, 11, 12, 13)
+    assert(result === expectResult)
+  }
 
 
   //2nd
@@ -15,4 +19,12 @@ class Assignment3Test extends FunSuite {
     val expectResult = Array(5, 6, 11, 12, 13)
     assert(result === expectResult)
   }
+
+  //3rd
+  test("Selection sort") {
+    val result = obj.selection(Array(12, 11, 13, 5, 6))
+    val expectResult = Array(5, 6, 11, 12, 13)
+    assert(result === expectResult)
+  }
+
 }
